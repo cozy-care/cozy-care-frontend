6f5ac2c6fd92d6846bfec7e434e95b05e2585e7d
Belle <bell71457145@gmail.com>
2024-08-20 08:05:50 +0700

    update information page

--- app/caregiver/[caregiverID]/page.tsx
+++ app/caregiver/[caregiverID]/page.tsx
@@ -11,7 +11,70 @@ export default function Caregiver() {
 
   return (
     <main className="flex flex-col min-h-[calc(100svh-3.5rem)]">
-      <div className="grow ">caregiverID Page</div>
+      <div className="grow ">
+        <h1 className="flex justify-end">ร่วมเป็นผู้ดูแลกับเรา</h1>
+
+        <form className="flex flex-col">
+          <div className="grid gap-6 mb-6 md:grid-cols-2 bg-blue-300 w-3/4 ">
+            <input
+              type="text"
+              id="first_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="ผู้ดูแล"
+              required
+            />
+            <input
+              type="text"
+              id="first_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="สถานะ : แพ็กเกจฟรี"
+              required
+            />
+            <input
+              type="text"
+              id="first_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="ตารางนัดหมาย"
+              required
+            />
+          </div>
+        </form>
+        <form>
+          <h2>กรอกข้อมูลผู้ดูแล</h2>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="last_name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                id="last_name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Doe"
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </div>
 
       <Footer />
     </main>
