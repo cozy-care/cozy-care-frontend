356bc5098cf526327fa5c293d42384267b4bc8cb
Belle <bell71457145@gmail.com>
2024-09-18 00:52:23 +0700

    remake register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import axios, { AxiosResponse } from "axios";
+import { Button } from "@nextui-org/react";
 
 interface RegisterFormState {
   username: string;
@@ -73,9 +74,9 @@ export default function Register() {
   };
 
   return (
-    <div className="flex w-screen h-screen static">
+    <div className="flex min-w-screen h-screen">
       <div className="w-5/12 h-full bg-slate-400 flex justify-end items-center">
-        <div className="bg-slate-200 p-4 rounded-l-xl w-3/4 h-4/5 flex flex-col justify-center items-center">
+        <div className="w-max h-[600px] bg-slate-200 p-4 rounded-l-xl  flex flex-col justify-start items-center ">
           <h1 className="flex justify-center text-indigo-700">
             เริ่มต้นใช้งาน
           </h1>
@@ -107,20 +108,23 @@ export default function Register() {
           </ul>
         </div>
       </div>
-      <div className="w-7/12 h-auto bg-slate-300 flex justify-start items-center">
-        <div className="bg-slate-100 p-4 rounded-r-xl w-4/5 h-4/5 flex justify-center">
-          <form onSubmit={handleSubmit}>
-            <fieldset>
-              <legend className="text-center my-7">
-                <h2 className="flex flex-col justify-center items-center text-3xl font-bold text-indigo-700">
-                  ลงทะเบียน
-                </h2>
-              </legend>
+
+      <div className="w-7/12 h-full bg-slate-300 flex justify-start  items-center ">
+        <form
+          onSubmit={handleSubmit}
+          className="w-[1000px] h-[600px] bg-slate-100  rounded-r-xl justify-start "
+        >
+          <fieldset className="flex-row justify-center items-center ">
+            <h2 className="flex justify-center text-3xl font-bold text-indigo-700 mt-4">
+              ลงทะเบียน
+            </h2>
+
+            <div className="flex flex-col justify-center items-center">
               {error && <p className="text-red-500">{error}</p>}
-              <div className="m-auto mb-2">
+              <div className=" mb-2">
                 <label
                   htmlFor="username"
-                  className="block text-sm font-medium text-gray-900"
+                  className="block text-lg font-medium text-gray-900"
                 >
                   ชื่อผู้ใช้
                 </label>
@@ -129,7 +133,7 @@ export default function Register() {
                   id="username"
                   value={formState.username}
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[600px] p-2.5"
                   placeholder="ชื่อผู้ใช้"
                   required
                 />
@@ -137,7 +141,7 @@ export default function Register() {
               <div className="mb-2">
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-gray-900"
+                  className="block text-lg  font-medium text-gray-900"
                 >
                   อีเมล
                 </label>
@@ -146,7 +150,7 @@ export default function Register() {
                   id="email"
                   value={formState.email}
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[600px] p-2.5"
                   placeholder="อีเมล"
                   required
                 />
@@ -154,7 +158,7 @@ export default function Register() {
               <div className="mb-2">
                 <label
                   htmlFor="password"
-                  className="block text-sm font-medium text-gray-900"
+                  className="block text-lg  font-medium text-gray-900"
                 >
                   รหัสผ่าน
                 </label>
@@ -163,7 +167,7 @@ export default function Register() {
                   id="password"
                   value={formState.password}
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[600px] p-2.5"
                   placeholder="รหัสผ่าน"
                   required
                 />
@@ -171,7 +175,7 @@ export default function Register() {
               <div className="mb-2">
                 <label
                   htmlFor="confirmPassword"
-                  className="block text-sm font-medium text-gray-900"
+                  className="block text-lg  font-medium text-gray-900"
                 >
                   ยืนยันรหัสผ่าน
                 </label>
@@ -180,84 +184,88 @@ export default function Register() {
                   id="confirmPassword"
                   value={formState.confirmPassword}
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[600px] p-2.5"
                   placeholder="ยืนยันรหัสผ่าน"
                   required
                 />
               </div>
-              <div className="flex justify-center mt-4">
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                >
-                  สมัคร
-                </button>
+              <div className="flex text-sm gap-x-[485px] items-center  mt-3 ">
+                <p>
+                  <a href="#">มีบัญชีแล้ว [LogIn]</a>
+                </p>
+                <p></p>
               </div>
-              <div className="inline-flex items-center justify-center w-full">
-                <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 mr-5" />
-                <span className="">หรือ</span>
-                <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 ml-5" />
-              </div>
-              <div className="flex justify-center mt-1">
-                <button
-                  className="flex items-center bg-white border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                  onClick={handleGoogleLogin}
+            </div>
+
+            <div className="flex justify-center mt-2">
+              <Button color="primary" className="text-lg w-[100px] h-[50px]">
+                สมัคร
+              </Button>
+            </div>
+            <div className="inline-flex items-center justify-center w-full">
+              <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 mr-5" />
+              <span className="">หรือ</span>
+              <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 ml-5" />
+            </div>
+            <div className="flex justify-center mt-1">
+              <button
+                className="flex items-center bg-white border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                onClick={handleGoogleLogin}
+              >
+                <svg
+                  className="h-6 w-6 mr-2"
+                  width="800px"
+                  height="800px"
+                  viewBox="-0.5 0 48 48"
+                  version="1.1"
                 >
-                  <svg
-                    className="h-6 w-6 mr-2"
-                    width="800px"
-                    height="800px"
-                    viewBox="-0.5 0 48 48"
-                    version="1.1"
+                  <title>Google-color</title>
+                  <desc>Created with Sketch.</desc>
+                  <defs> </defs>
+                  <g
+                    id="Icons"
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
                   >
-                    <title>Google-color</title>
-                    <desc>Created with Sketch.</desc>
-                    <defs> </defs>
                     <g
-                      id="Icons"
-                      stroke="none"
-                      strokeWidth="1"
-                      fill="none"
-                      fillRule="evenodd"
+                      id="Color-"
+                      transform="translate(-401.000000, -860.000000)"
                     >
                       <g
-                        id="Color-"
-                        transform="translate(-401.000000, -860.000000)"
+                        id="Google"
+                        transform="translate(401.000000, 860.000000)"
                       >
-                        <g
-                          id="Google"
-                          transform="translate(401.000000, 860.000000)"
-                        >
-                          <path
-                            d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-                            id="Fill-1"
-                            fill="#FBBC05"
-                          ></path>
-                          <path
-                            d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-                            id="Fill-2"
-                            fill="#EB4335"
-                          ></path>
-                          <path
-                            d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-                            id="Fill-3"
-                            fill="#34A853"
-                          ></path>
-                          <path
-                            d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-                            id="Fill-4"
-                            fill="#4285F4"
-                          ></path>
-                        </g>
+                        <path
+                          d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                          id="Fill-1"
+                          fill="#FBBC05"
+                        ></path>
+                        <path
+                          d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                          id="Fill-2"
+                          fill="#EB4335"
+                        ></path>
+                        <path
+                          d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                          id="Fill-3"
+                          fill="#34A853"
+                        ></path>
+                        <path
+                          d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                          id="Fill-4"
+                          fill="#4285F4"
+                        ></path>
                       </g>
                     </g>
-                  </svg>
-                  <span>ลงทะเบียน ด้วย Google</span>
-                </button>
-              </div>
-            </fieldset>
-          </form>
-        </div>
+                  </g>
+                </svg>
+                <span>ลงทะเบียน ด้วย Google</span>
+              </button>
+            </div>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
