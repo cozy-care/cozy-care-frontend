620ab80096c9920f000dc822ab130054b42d9e1c
Belle <bell71457145@gmail.com>
2025-02-06 22:54:51 +0700

    responsive and submit button add caregiver

--- app/caregiver/add/page.tsx
+++ app/caregiver/add/page.tsx
@@ -14,6 +14,8 @@ import { services, employments } from "@/app/caregiver/add/addMock";
 import { ArrowBackIosNew } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 
+import { LocationOn, Edit } from "@mui/icons-material";
+
 export default function Add() {
   const router = useRouter();
   useEffect(() => {
@@ -22,7 +24,7 @@ export default function Add() {
 
   return (
     <main className="flex flex-col min-h-[100dvh]">
-      <div className="grow flex flex-col items-center px-6 gap-6">
+      <div className="grow flex flex-col items-center px-6 gap-6 mb-6">
         <div className="flex relative items-center gap-3 w-full h-[50px]">
           <Button
             as={Link}
@@ -38,7 +40,7 @@ export default function Add() {
             เพิ่มข้อมูลผู้ดูแล
           </h1>
         </div>
-        <div className="flex flex-col w-full gap-1">
+        <div className="flex flex-col w-full md:w-[40%] lg:w-[50%] gap-1">
           <h2>เลือกบริการที่ต้องการรับการดูแล</h2>
           <Select
             className="w-full"
@@ -50,7 +52,7 @@ export default function Add() {
             ))}
           </Select>
         </div>
-        <div className="bg-cozy-green-dark rounded-full flex flex-row p-2 px-3 gap-2 justify-between items-center w-full">
+        <div className="bg-cozy-green-dark rounded-full flex flex-row p-2 px-3 gap-2 justify-between items-center w-full md:w-[40%] lg:w-[50%]">
           <p className="w-max text-nowrap">เริ่มต้น</p>
           <DatePicker radius="full" size="sm" className="grow"></DatePicker>
           <TimeInput
@@ -61,7 +63,7 @@ export default function Add() {
             className="w-max"
           ></TimeInput>
         </div>
-        <div className="bg-cozy-green-dark rounded-full flex flex-row p-2 px-3 gap-2 justify-between items-center w-full">
+        <div className="bg-cozy-green-dark rounded-full flex flex-row p-2 px-3 gap-2 justify-between items-center w-full md:w-[40%] lg:w-[50%]">
           <p className="w-max text-nowrap">สิ้นสุด</p>
           <DatePicker radius="full" size="sm" className="grow"></DatePicker>
           <TimeInput
@@ -73,9 +75,11 @@ export default function Add() {
           ></TimeInput>
         </div>
 
-        <div className="w-full flex flex-col justify-center items-center  rounded-full">
-          <p className="w-full flex bg-cozy-blue-light rounded-t-xl rounded-b-none p-1 justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center  rounded-full md:w-[40%] lg:w-[50%]">
+          <p className="w-full flex bg-cozy-blue-light rounded-t-xl p-1 justify-between items-center px-4">
+            <LocationOn style={{ width: "30px", height: "auto" }} />
             เลือกตำแหน่งที่ต้องการจ้างงาน
+            <Edit style={{ width: "30px", height: "auto" }} />
           </p>
 
           <img
@@ -83,13 +87,15 @@ export default function Add() {
             alt="map"
             className="w-full h-40 object-cover"
           />
-          <p className="w-full flex text-black text-wrap bg-white rounded-t-none rounded-b-xl p-1 ">
-            99/1 ซอย8 หมู่4 ถนนเสนา บางนา บางนา กรุงเทพมหานคร 10200
-          </p>
+          <div className="w-full flex flex-wrap justify-center items-center text-black bg-white rounded-b-xl p-1 ">
+            <p className="w-full flex-wrap px-4">
+              99/1 ซอย8 หมู่4 ถนนเสนา บางนา บางนา กรุงเทพมหานคร 10200
+            </p>
+          </div>
         </div>
-        <div className="w-full flex flex-col gap-2">
-          <h3 className="font-bold gap-2">ความต้องการเพิ่มเติม</h3>
-          <div className="text-black flex flex-col w-full h-full gap-2 bg-cozy-lightblue-light rounded-3xl p-2">
+        <div className="w-full flex flex-col gap-2 md:w-[40%] lg:w-[50%]">
+          <h3 className="font-bold gap-1">ความต้องการเพิ่มเติม</h3>
+          <div className="text-black flex flex-col w-full  h-full gap-1 bg-cozy-lightblue-light rounded-2xl p-2">
             <p>การจ้างงาน</p>
             <Select
               className="w-full"
@@ -103,14 +109,20 @@ export default function Add() {
             <p className="w-full">ราคา</p>
             <Input
               className="w-full"
-              placeholder="เช่น 800-1600บาท/วัน , 9000-15000บาท/เดือน, ไม่ระบุ"
+              placeholder="เช่น 800-1,600บาท/วัน , 9,000-15,000บาท/เดือน, ไม่ระบุ"
             ></Input>
           </div>
         </div>
-        <div className="flex flex-col w-full h-full gap-2 bg-[#1F5670] rounded-3xl p-2">
+        <div className="flex flex-col w-full h-full gap-1 bg-[#1F5670] rounded-2xl p-2 md:w-[40%] lg:w-[50%]">
           <h4>ความสามารถเพิ่มเติม</h4>
-          <Input className="" placeholder="เช่น ขับรถ(มีรถ) ทำอาหาร"></Input>
+          <Input className="" placeholder="เช่น ขับรถ (มีรถ), ทำอาหาร"></Input>
         </div>
+        <Button
+          radius="full"
+          className="text-lg font-semibold bg-cozy-green-light px-16 lg:px-20"
+        >
+          ยืนยัน
+        </Button>
       </div>
     </main>
   );
