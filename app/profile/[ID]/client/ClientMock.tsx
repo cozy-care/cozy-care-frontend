7d1ed0c55418f882c84af8841f183108a117a67b
Belle <bell71457145@gmail.com>
2025-02-18 03:16:05 +0700

    ClientMock data

---
+++ app/profile/[ID]/client/ClientMock.tsx
@@ -0,0 +1,11 @@
+export interface ClientProps {
+  firstname?: string;
+  lastname?: string;
+}
+
+export const clientMock: ClientProps[] = [
+  { firstname: "นาย ริดเดช" },
+  { firstname: "ผู้รับการดูแล 2" },
+  { firstname: "สองสี", lastname: "สามใบ" },
+  { firstname: "ผู้รับการดูแล 4" },
+];
--- app/profile/[ID]/client/page.tsx
+++ app/profile/[ID]/client/page.tsx
@@ -1,18 +1,53 @@
-'use client'
+"use client";
 
+import {
+  Button,
+  DatePicker,
+  Input,
+  Link,
+  Select,
+  SelectItem,
+  TimeInput,
+} from "@nextui-org/react";
 import { useEffect } from "react";
+import { services, employments } from "@/app/caregiver/add/addMock";
+import { ArrowBackIosNew } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
+
+import { Delete } from "@mui/icons-material";
 
 export default function UserClient() {
+  const router = useRouter();
+  useEffect(() => {
+    document.title = "Client - Cozy Care";
+  }, []);
 
-    useEffect(() => {
-        document.title = "Client - Cozy Care";
-    }, []);
-
-    return (
-        <main className="flex flex-col min-h-[100dvh]">
-            <div className="grow flex flex-col items-center">
-                Client page
-            </div>
-        </main>
-    )
+  return (
+    <main className="flex flex-col min-h-[100dvh]">
+      <div className="grow flex flex-col items-center px-6 gap-6 mb-6">
+        <div className="flex relative items-center gap-3 w-full h-[50px]">
+          <Button
+            as={Link}
+            onPress={() => router.back()}
+            className="text-cozy-green-light"
+            isIconOnly
+            radius="full"
+            variant="light"
+          >
+            <ArrowBackIosNew />
+          </Button>
+          <h1 className="absolute  w-full flex text-lg font-bold justify-center items-center ">
+            ผู้รับการดูแล
+          </h1>
+        </div>
+        <hr className="w-full  border-t-8 " />
+        <div className="flex flex-col w-[80%] md:w-[50%] lg:w-[30%] rounded-full gap-y-5">
+          <Button className="justify-between">
+            นาย ริดเดช <Delete style={{ width: "25px", height: "auto" }} />
+          </Button>
+          <Button>ผู้รับการดูแล 2</Button>
+        </div>
+      </div>
+    </main>
+  );
 }
