74021d17eec01726a6a0fef3ebc6b8920806dcb2
KowMunGai <koonew@hotmail.com>
2025-02-23 21:12:28 +0700

    Appointment card updated

--- app/messages/[id]/info/privacy_and_support/about/appointment_history/page.tsx
+++ app/messages/[id]/info/privacy_and_support/about/appointment_history/page.tsx
@@ -4,20 +4,17 @@ import { ArrowBackIosNew, History, ThumbDown, ThumbUp } from "@mui/icons-materia
 import { Button } from "@nextui-org/react";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import AppointmentCard from "./AppointmentCard";
 
 export default function AppointmentHistory() {
   const router = useRouter();
   const { id: chatId } = useParams() as { id: string };
-  const [isToggle, setIsToggle] = useState<boolean>(false);
 
   useEffect(() => {
     document.title = "History - Cozy Care";
   }, []);
 
-  
-  const cardToggle = () => {
-    setIsToggle(!isToggle);
-  };
+
 
   return (
     <main className="flex flex-col min-h-[100dvh]">
@@ -39,18 +36,13 @@ export default function AppointmentHistory() {
         </div>
 
         <div className="flex flex-col w-full gap-6 px-4">
-          <Button onPress={cardToggle} disableAnimation radius="md" className="flex justify-start w-full h-max bg-white shadow-lg p-3 gap-3">
-            <div className="w-[30px] h-auto aspect-square flex self-start"><History sx={{width:'auto', height:'auto'}}/></div>
-
-            <div className="flex flex-col items-start h-max gap-2 text-base">
-              <p>วันที่ 11 ธันวาคม 2024</p>
-              <p>รายละเอียดงาน : พาไปหาหมอ</p>
-              <p>ราคา : 1000 บาท/วัน</p>
-            </div>
-
-            <div className="w-[30px] h-auto aspect-square text-[#09D810] self-end ml-auto"><ThumbUp sx={{width:'auto', height:'auto'}}/></div>
-            {/* <div className="w-[30px] h-auto aspect-square text-[#FF0101] self-end ml-auto"><ThumbDown sx={{width:'auto', height:'auto'}}/></div> */}
-          </Button>
+          <AppointmentCard
+            date="11 ธันวาคม 2024"
+            detail="123"
+            cost={1000}
+            status="Ongoing"
+            remark="232"
+          />
         </div>
       </div>
 
